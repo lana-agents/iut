46bd676e0baf1ac82d7087bb728887b2972d8b18
Christian Merten <lana@merten.dev>
2026-09-05 16:59:06 +0000

    Iut.lean: regenerate import list

--- Iut.lean
+++ Iut.lean
@@ -97,11 +97,7 @@ import Iut.Tripod.Height
 import Iut.Tripod.Legendre
 import Iut.Tripod.LogCond
 import Iut.Tripod.Main
-import Iut.Tripod.Main
-import Iut.Tripod.Main
 import Iut.Tripod.Northcott
-import Iut.Tripod.Providers
-import Iut.Tripod.Providers
 import Iut.Tripod.Providers
 import Iut.Tripod.SL2Generation
 import Iut.Tripod.StableOdd
